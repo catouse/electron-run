eb2ff5fa9e6f4c927fbbb0271eac602415735cff
Catouse <catouse@gmail.com>
2018-01-04 10:12:31 +0800

    * change build files.

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -17,6 +17,9 @@ export default validate({
         }, {
             test: /\.json$/,
             loader: 'json-loader'
+        }, {
+            test: /\.css$/,
+            loader: 'style!css'
         }],
         noParse: [/ajv/]
     },
